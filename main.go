735de8574a51a0b49b63d5411c45f77f03b4f8ea
Chris Gansen <cgansen@gmail.com>
2014-09-02 00:17:23 -0500

    concurrent download

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 
 // download a list of items that match a tag
 func snarf(tag, clientID string) error {
-	url := fmt.Sprintf("https://api.instagram.com/v1/tags/%s/media/recent?client_id=%s", tag, clientID)
+	url := fmt.Sprintf("https://api.instagram.com/v1/tags/%s/media/recent?count=500&client_id=%s", tag, clientID)
 
 	fmt.Println("GET %s", url)
 	resp, err := http.Get(url)
@@ -64,33 +64,56 @@ func snarf(tag, clientID string) error {
 		return err
 	}
 
-	download(s.Assets[0], "files")
+	work := make(chan Asset, len(s.Assets))
+	done := make(chan error, 5)
+
+	// launch five downloaders
+	for i := 0; i < 5; i++ {
+		go download(work, done, "files")
+	}
+
+	// send items to workers
+	for _, a := range s.Assets {
+		work <- a
+	}
+	close(work)
+
+	for i := 0; i < 5; i++ {
+		err := <-done
+		if err != nil {
+			// handle
+		}
+	}
 
 	return nil
 }
 
 // trigger a download of an asset
-func download(a Asset, out string) error {
-	path := fmt.Sprintf("./%s/%s-%s.jpg", out, a.User.Username, a.ID)
+func download(assets <-chan Asset, done chan<- error, out string) error {
+	for a := range assets {
+		path := fmt.Sprintf("./%s/%s-%s.jpg", out, a.User.Username, a.ID)
 
-	imgUrl := a.Images["standard_resolution"].URL
-	resp, err := http.Get(imgUrl)
-	if err != nil {
-		fmt.Println("error: fetch img failed:", err)
-		return err
+		imgUrl := a.Images["standard_resolution"].URL
+		fmt.Println("downloading", imgUrl)
+		resp, err := http.Get(imgUrl)
+		if err != nil {
+			fmt.Println("error: fetch img failed:", err)
+			return err
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Println("error: read response failed:", err)
+			return err
+		}
+
+		if err := ioutil.WriteFile(path, body, 0644); err != nil {
+			fmt.Println("error: could not write file:", err)
+			return err
+		}
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		fmt.Println("error: read response failed:", err)
-		return err
-	}
-
-	if err := ioutil.WriteFile(path, body, 0644); err != nil {
-		fmt.Println("error: could not write file:", err)
-		return err
-	}
-
+	done <- nil
 	return nil
 }
